2abfd13e9536010bc0faf2906699d4fbd0c94af1
Jelle Vegter <j.vegter@vesteda.com>
2021-11-21 17:49:31 +0100

    tests

--- test/test_players.py
+++ test/test_players.py
@@ -5,7 +5,6 @@ from src.players import (
     retrieve_player_data,
     calculate_points,
 )
-import test
 from test.mock_objects import MOCK_TEAM, MOCK_PLAYER1, MOCK_PLAYER2
 import pytest
 
@@ -48,34 +47,72 @@ def test_retrieve_player_scores(col, value) -> None:
         ("last_30_2022", "avg", 41.22),
     ],
 )
-def test_calculate_points(col, type, value) -> None:
+def test_calculate_points_player1(col, type, value) -> None:
     assert calculate_points(MOCK_PLAYER1.stats[col][type]) == value
 
 
-# def test_retrieve_player_info() -> None:
-#     data = player_info(MOCK_TEAM)
-#     assert data.shape[0] == len(MOCK_TEAM)
-#     assert data.shape[1] == 4
-#     assert data.loc[0, "Team"] == "BKN"
-#     assert data.loc[0, "Position"] == "PG"
-#     assert data.loc[0, "Status"] == "ACTIVE"
-#     assert data.loc[1, "Team"] == "TOR"
-#     assert data.loc[1, "Position"] == "SG"
-#     assert data.loc[1, "Status"] == "ACTIVE"
-#
-#
-# def test_retrieve_player_data() -> None:
-#     data = retrieve_player_data(MOCK_TEAM)
-#     print(data)
-#     assert data["Score"].sum() == 82.07
-#     assert data["Fs.2022.avg"].sum() == 76.78999999999999
-#     assert data["Fs.2022.total"].sum() == 620.0
-#     assert data["Proj.2022.avg"].sum() == 93.5
-#     assert data["Proj.2022.total"].sum() == 7147.0
-#     assert data["30_d.2022.avg"].sum() == 76.78999999999999
-#     assert data.loc[0, "Team"] == "BKN"
-#     assert data.loc[0, "Position"] == "PG"
-#     assert data.loc[0, "Status"] == "ACTIVE"
-#     assert data.loc[1, "Team"] == "TOR"
-#     assert data.loc[1, "Position"] == "SG"
-#     assert data.loc[1, "Status"] == "ACTIVE"
+@pytest.mark.parametrize(
+    ("col", "type", "value"),
+    [
+        ("total_2022", "avg", 35.57),
+        ("projected_total_2022", "total", 3189.0),
+        ("last_7_2022", "total", 63.0),
+        ("last_15_2022", "avg", 35.67),
+        ("last_30_2022", "total", 249.0),
+    ],
+)
+def test_calculate_points_player2(col, type, value) -> None:
+    assert calculate_points(MOCK_PLAYER2.stats[col][type]) == value
+
+
+@pytest.mark.parametrize(
+    ("index", "col", "value"),
+    [
+        (0, "Team", "BKN"),
+        (0, "Position", "PG"),
+        (0, "Status", "ACTIVE"),
+        (1, "Team", "TOR"),
+        (1, "Position", "SG"),
+        (1, "Status", "ACTIVE"),
+    ],
+)
+def test_retrieve_player_info(index, col, value) -> None:
+    data = player_info(MOCK_TEAM)
+    assert data.shape[0] == len(MOCK_TEAM)
+    assert data.shape[1] == 4
+    assert data.loc[index, col] == value
+
+
+@pytest.mark.parametrize(
+    ("col", "value"),
+    [
+        ("Score", 80.06),
+        ("total_2022.avg", 76.78999999999999),
+        ("total_2022.total", 620.0),
+        ("projected_total_2022.avg", 93.5),
+        ("projected_total_2022.total", 7147.0),
+        ("last_30_2022.avg", 76.78999999999999),
+    ],
+)
+def test_retrieve_player_data_values(col, value) -> None:
+    data = retrieve_player_data(MOCK_TEAM)
+    assert data[col].sum() == value
+
+
+@pytest.mark.parametrize(
+    ("index", "col", "value"),
+    [
+        (0, "Team", "BKN"),
+        (0, "Position", "PG"),
+        (0, "Status", "ACTIVE"),
+        (1, "Team", "TOR"),
+        (1, "Position", "SG"),
+        (1, "Status", "ACTIVE"),
+    ],
+)
+def test_retrieve_player_data_info(index, col, value) -> None:
+    data = retrieve_player_data(MOCK_TEAM)
+    assert data.loc[index, col] == value
+
+
+### TODO check denver not found
